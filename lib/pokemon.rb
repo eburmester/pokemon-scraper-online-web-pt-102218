7e59215ed85281012bc66ccc64af482af8af79d7
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-23 02:47:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,9 @@
 class Pokemon
   
-  @@all = []
+  attr_accessor :id, :name, :type, :db
   
-  def initialize(id:, name:, type:, db:)
-    # @id = id
-    # @name = name
-    # @type = type
-    # @db = db
-    # @@all << self 
+  def initialize(keywords)
+    
   end
     
   def self.all
