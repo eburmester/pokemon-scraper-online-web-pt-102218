cec355513b595f063804d66f0c7862ae9dcfdbc2
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-23 02:36:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  
+  @@all = []
+  
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self 
+  end
+    
+  def self.all
+    @@all
+  end
+  
+  def self.save(name, type)
+    @db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+  
 end
